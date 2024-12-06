b0fb29460c4b79863e1e2085557880717d2e8438
danazou <52993274+danazou@users.noreply.github.com>
2024-12-06 19:12:12 +0000

    day 2 part 2 solution

--- aoc-2024/day-02/part-2.js
+++ aoc-2024/day-02/part-2.js
@@ -15,72 +15,38 @@ fs.readFile(file, "utf8", (err, data) => {
 		// convert string to int
 		reports.push(string.split(" ").map((x) => parseInt(x)));
 	}
-
-	let count = 0;
-	for (const report of reports) {
-		// console.log("new report", report);
-		let is_increasing = report[0] < report[1];
-		let is_safe = false;
-		let bad_levels = 0;
-		let removals = {
-			dropped: null,
-			potential: null,
-			index: null,
-		};
-		const check_safety = (prev, num) => {
-			const diff = Math.abs(num - prev);
-			const is_consistent = num > prev == is_increasing;
-			const has_adequate_diff = diff > 0 && diff < 4;
-			is_safe = is_consistent && has_adequate_diff;
-		};
-		// look through each level of the report
+	const check_safety = (report) => {
+		const is_increasing = report[0] < report[1];
+		let safe;
 		for (let i = 1; i < report.length; i++) {
 			// start at 1st num instead of 0th
 			const num = report[i];
 			const prev = report[i - 1];
-
-			// check if report is safe
 			const diff = Math.abs(num - prev);
 			const is_consistent = num > prev == is_increasing;
 			const has_adequate_diff = diff > 0 && diff < 4;
-			is_safe = is_consistent && has_adequate_diff;
+			safe = is_consistent && has_adequate_diff;
+			if (!safe) {
+				break;
+			}
+		}
+		return safe;
+	};
+	let count = 0;
+	for (const report of reports) {
+		let is_safe = check_safety(report);
 
-			if (!is_safe) {
-				console.log(
-					report,
-					"not safe because!",
-					prev,
-					num,
-					is_consistent ? "bad difference" : "inconsistent"
-				);
-				bad_levels += 1;
-				console.log(bad_levels);
-				// only remove first unsafe level
-				if (bad_levels == 1) {
-					console.log("remove a level and try again");
-					removals = { dropped: prev, potential: num, index: i - 1 };
-					// delete prev
-					report.splice(removals.index, 1);
-					// reset
-					is_increasing = report[0] < report[1];
-					i = 0;
-				} else if (bad_levels == 2) {
-					console.log("remove the other level and try again");
-					// swap dropped and potential to create new report option
-					report.splice(removals.index, 1, removals.dropped);
-					// reset
-					is_increasing = report[0] < report[1];
-					i = 0;
-				} else if (bad_levels > 2) {
-					console.log("uh oh, unsafe regardless of which level removed");
+		if (!is_safe) {
+			for (let i = 0; i < report.length; i++) {
+				alternative = report.toSpliced(i, 1);
+				is_safe = check_safety(alternative);
+				if (is_safe) {
 					break;
 				}
 			}
 		}
-		// if (is_safe) {
-		// 	console.log(report, "is safe!");
-		// }
 		count += is_safe ? 1 : 0;
 	}
 	console.log(count);
+	// 398
 });
