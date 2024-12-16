dae7150f50a4f7a6183febec9add723786cca08a
danazou <52993274+danazou@users.noreply.github.com>
2024-12-16 23:43:36 +0000

    refactor day 4 part 2

--- aoc-2024/day-04/part-2.js
+++ aoc-2024/day-04/part-2.js
@@ -3,6 +3,28 @@ let file = "input.txt";
 let input = "";
 let count = 0;
 
+const find_cross_mas = (index, word_search) => {
+	const i = index[0];
+	const j = index[1];
+	const rows = word_search.length;
+	const columns = word_search[0].length;
+
+	if (i == 0 || i == rows - 1 || j == 0 || j == columns - 1) {
+		return 0;
+	}
+	// look at positive and negative diagonal
+	const pos_diag = word_search[i - 1][j - 1] + "A" + word_search[i + 1][j + 1];
+	const neg_diag = word_search[i + 1][j - 1] + "A" + word_search[i - 1][j + 1];
+
+	if (
+		(pos_diag == "MAS" || pos_diag == "SAM") &&
+		(neg_diag == "MAS" || neg_diag == "SAM")
+	) {
+		return 1;
+	}
+	return 0;
+};
+
 fs.readFile(file, "utf8", (err, data) => {
 	if (err) {
 		console.error("Error reading the file", err);
@@ -15,41 +37,17 @@ fs.readFile(file, "utf8", (err, data) => {
 	for (line of input) {
 		word_search.push(line.split(""));
 	}
-	let indexes = [];
-	const rows = word_search.length;
-	const columns = word_search[0].length;
+
 	// find all positions of A's and go from there
-	for (let i = 0; i < rows; i++) {
-		for (let j = 0; j < columns; j++) {
+	for (let i = 0; i < word_search.length; i++) {
+		for (let j = 0; j < word_search[0].length; j++) {
 			const letter = word_search[i][j];
 			if (letter == "A") {
-				indexes.push([i, j]);
+				count += find_cross_mas([i, j], word_search);
 			}
 		}
 	}
 
-	for (index of indexes) {
-		const i = index[0];
-		const j = index[1];
-
-		if (i == 0 || i == rows - 1 || j == 0 || j == columns - 1) {
-			continue;
-		} else {
-			// look at positive and negative diagonal
-			const pos_diag =
-				word_search[i - 1][j - 1] + "A" + word_search[i + 1][j + 1];
-			const neg_diag =
-				word_search[i + 1][j - 1] + "A" + word_search[i - 1][j + 1];
-			// console.log(pos_diag, neg_diag);
-
-			if (
-				(pos_diag == "MAS" || pos_diag == "SAM") &&
-				(neg_diag == "MAS" || neg_diag == "SAM")
-			) {
-				count += 1;
-			}
-		}
-	}
 	console.log(count);
 	// 1930
 });
