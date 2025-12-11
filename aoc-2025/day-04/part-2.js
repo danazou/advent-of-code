8a0b03e23afb251317ca6234ca9725864a66e45e
danazou <52993274+danazou@users.noreply.github.com>
2025-12-11 20:11:46 +0000

    day 4 part 2 solution

--- aoc-2025/day-04/part-2.js
+++ aoc-2025/day-04/part-2.js
@@ -18,7 +18,8 @@ function check_roll_count(location, max_row, max_column) {
 				row + i < 0 ||
 				column + j < 0 ||
 				row + i >= max_row ||
-				column + j >= max_column
+				column + j >= max_column ||
+				(i == 0 && j == 0)
 			) {
 				// out of bounds
 				continue;
@@ -29,20 +30,37 @@ function check_roll_count(location, max_row, max_column) {
 			}
 		}
 	}
-	return roll_count - 1; // -1 as we're also counting itself
+	return roll_count;
 }
 
-for (let i = 0; i < input.length; i++) {
-	for (let j = 0; j < input[i].length; j++) {
-		if (input[i][j] == "@") {
-			// toilet roll found
-			if (check_roll_count([i, j], input.length, input[i].length) < 4) {
-				// fork lift accessible
-				sum += 1;
+let accessible_rolls = 0;
+let has_accessible_rolls = true;
+
+while (has_accessible_rolls == true) {
+	let next_state = JSON.parse(JSON.stringify(input));
+
+	for (let i = 0; i < input.length; i++) {
+		for (let j = 0; j < input[i].length; j++) {
+			if (input[i][j] == "@") {
+				// toilet roll found
+				if (check_roll_count([i, j], input.length, input[i].length) < 4) {
+					// fork lift accessible
+					sum += 1;
+					accessible_rolls += 1;
+					next_state[i][j] = "x";
+				}
 			}
 		}
 	}
+
+	if (accessible_rolls > 0) {
+		// reset counter
+		accessible_rolls = 0;
+		input = next_state;
+	} else {
+		has_accessible_rolls = false;
+	}
 }
 
-// recursive?
 console.log(sum);
+// 8317
