a624b14867448d44b7a79cd202b6643f6aab5c6f
danazou <52993274+danazou@users.noreply.github.com>
2025-12-11 20:19:26 +0000

    slight improvements to day 4 part 2

--- aoc-2025/day-04/part-2.js
+++ aoc-2025/day-04/part-2.js
@@ -3,12 +3,11 @@ import input_str from "./input.js";
 
 let input = input_str.split("\n").map((x) => x.split(""));
 
-let sum = 0;
-
-function check_roll_count(location, max_row, max_column) {
+function get_roll_count(location) {
 	let row = location[0];
 	let column = location[1];
-
+	let max_row = input.length;
+	let max_column = input[0].length;
 	let roll_count = 0;
 
 	// look at adjacent co-ordinates
@@ -33,22 +32,20 @@ function check_roll_count(location, max_row, max_column) {
 	return roll_count;
 }
 
+let sum = 0;
 let accessible_rolls = 0;
 let has_accessible_rolls = true;
 
-while (has_accessible_rolls == true) {
+while (has_accessible_rolls) {
 	let next_state = JSON.parse(JSON.stringify(input));
 
 	for (let i = 0; i < input.length; i++) {
 		for (let j = 0; j < input[i].length; j++) {
-			if (input[i][j] == "@") {
-				// toilet roll found
-				if (check_roll_count([i, j], input.length, input[i].length) < 4) {
-					// fork lift accessible
-					sum += 1;
-					accessible_rolls += 1;
-					next_state[i][j] = "x";
-				}
+			if (input[i][j] == "@" && get_roll_count([i, j]) < 4) {
+				// toilet roll found && fork lift accessible
+				sum += 1;
+				accessible_rolls += 1;
+				next_state[i][j] = "x";
 			}
 		}
 	}
