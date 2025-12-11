340722ac179671a1b90ddeb57f00328384ada288
danazou <52993274+danazou@users.noreply.github.com>
2025-12-11 20:42:32 +0000

    refactor day 4 part 2

--- aoc-2025/day-04/part-2.js
+++ aoc-2025/day-04/part-2.js
@@ -3,29 +3,19 @@ import input_str from "./input.js";
 
 let input = input_str.split("\n").map((x) => x.split(""));
 
-function get_roll_count(location) {
-	let row = location[0];
-	let column = location[1];
-	let max_row = input.length;
-	let max_column = input[0].length;
+function get_roll_count(grid, [row, column]) {
 	let roll_count = 0;
 
 	// look at adjacent co-ordinates
 	for (let i = -1; i < 2; i++) {
 		for (let j = -1; j < 2; j++) {
-			if (
-				row + i < 0 ||
-				column + j < 0 ||
-				row + i >= max_row ||
-				column + j >= max_column ||
-				(i == 0 && j == 0)
-			) {
-				// out of bounds
+			if (i == 0 && j == 0) {
+				// skip itself
 				continue;
-			} else {
-				if (input[row + i][column + j] == "@") {
-					roll_count += 1;
-				}
+			}
+
+			if (grid[row + i]?.[column + j] == "@") {
+				roll_count += 1;
 			}
 		}
 	}
@@ -33,15 +23,19 @@ function get_roll_count(location) {
 }
 
 let sum = 0;
-let accessible_rolls = 0;
-let has_accessible_rolls = true;
+let current_state = JSON.parse(JSON.stringify(input));
 
-while (has_accessible_rolls) {
-	let next_state = JSON.parse(JSON.stringify(input));
+while (true) {
+	// keep checking until no more accessible toilet rolls
+	let accessible_rolls = 0;
+	let next_state = JSON.parse(JSON.stringify(current_state));
 
-	for (let i = 0; i < input.length; i++) {
-		for (let j = 0; j < input[i].length; j++) {
-			if (input[i][j] == "@" && get_roll_count([i, j]) < 4) {
+	for (let i = 0; i < current_state.length; i++) {
+		for (let j = 0; j < current_state[i].length; j++) {
+			if (
+				current_state[i][j] == "@" &&
+				get_roll_count(current_state, [i, j]) < 4
+			) {
 				// toilet roll found && fork lift accessible
 				sum += 1;
 				accessible_rolls += 1;
@@ -50,13 +44,13 @@ while (has_accessible_rolls) {
 		}
 	}
 
-	if (accessible_rolls > 0) {
-		// reset counter
-		accessible_rolls = 0;
-		input = next_state;
-	} else {
-		has_accessible_rolls = false;
+	if (accessible_rolls == 0) {
+		break;
 	}
+
+	// reset counter
+	accessible_rolls = 0;
+	current_state = next_state;
 }
 
 console.log(sum);
