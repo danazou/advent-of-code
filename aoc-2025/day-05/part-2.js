536827e11d43f46b5d35e5ac304555e3cdf9e7aa
danazou <52993274+danazou@users.noreply.github.com>
2025-12-19 22:39:48 +0000

    part 2 solution

--- aoc-2025/day-05/part-2.js
+++ aoc-2025/day-05/part-2.js
@@ -1,39 +1,53 @@
 import test from "./test.js";
 import input_str from "./input.js";
 
-let input = test.split("\n\n").map((x) => x.split("\n"));
+let input = input_str.split("\n\n").map((x) => x.split("\n"));
 let fresh_range = input[0].map((x) => x.split("-").map((x) => parseInt(x)));
 
-let count = 0;
-let unique_ranges = [];
+let ranges = [];
 
-function find_unique_range([start, end], unique_ranges) {
-	let new_start = start;
-	let new_end = end;
-
-	for (let i = 0; i < unique_ranges.length; i++) {
-		let [range_start, range_end] = unique_ranges[i];
-
-		if (new_start >= range_start && new_start <= range_end) {
-			// start is between existing range
-			new_start = range_end + 1;
+function check_range(range, ranges) {
+	let is_overlapping = false;
+	let [start, end] = range;
+	for (let i = 0; i < ranges.length; i++) {
+		// check if there's overlap
+		let [range_start, range_end] = ranges[i];
+		if (start > range_end || end < range_start) {
+			continue;
 		}
 
-		if (new_end >= range_start && new_end <= range_end) {
-			// end is between existing range
-			new_end = range_start - 1;
+		is_overlapping = true;
+		// if overlapping, update range
+		if (start < range_start) {
+			ranges[i][0] = start;
+		}
+
+		if (end > range_end) {
+			ranges[i][1] = end;
 		}
 	}
-
-	return [new_start, new_end];
+	return is_overlapping;
 }
 
-for (let i = 0; i < fresh_range.length; i++) {
-	let unique_range = find_unique_range(fresh_range[i], unique_ranges);
-	unique_ranges.push(unique_range);
-	count += unique_range[1] - unique_range[0] + 1;
+while (true) {
+	for (let i = 0; i < fresh_range.length; i++) {
+		if (check_range(fresh_range[i], ranges)) {
+			continue;
+		}
+		ranges.push(fresh_range[i]);
+	}
+
+	if (ranges.length >= fresh_range.length) {
+		break;
+	}
+	fresh_range = ranges;
+	ranges = [];
 }
-console.log(unique_ranges);
+
+let count = 0;
+for (let i = 0; i < ranges.length; i++) {
+	count += ranges[i][1] - ranges[i][0] + 1;
+}
+
 console.log(count);
-// 262170254992853 too low
-// 256760477268802
+// 344378119285354
