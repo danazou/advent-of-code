02ec5ee82d270d89cad63238a00833562a382918
danazou <52993274+danazou@users.noreply.github.com>
2025-12-20 17:01:43 +0000

    refactor part 2

--- aoc-2025/day-06/part-2.js
+++ aoc-2025/day-06/part-2.js
@@ -1,24 +1,25 @@
 import test from "./test.js";
 import input_str from "./input.js";
 
-function get_indices_of_char(string) {
-	let indices = [];
-	for (let i = 0; i < string.length; i++) {
-		if (string[i] == " ") {
-			continue;
-		}
-		indices.push(i);
-	}
-	return indices;
+function get_non_space_indices(string) {
+	return [...string].flatMap((char, i) => (char !== " " ? [i] : []));
+	// let indices = [];
+	// for (let i = 0; i < string.length; i++) {
+	// 	if (string[i] == " ") {
+	// 		continue;
+	// 	}
+	// 	indices.push(i);
+	// }
+	// return indices;
 }
 
-function split_string(string, indices) {
+function split_string(lines, indices) {
 	let post_split = [];
-	for (let i = 0; i < string.length; i++) {
+	for (let i = 0; i < lines.length; i++) {
 		let nums = [];
 		for (let j = 0; j < indices.length; j++) {
 			let [start, end] = [indices[j], indices[j + 1]];
-			nums.push(string[i].substring(start, end));
+			nums.push(lines[i].substring(start, end));
 		}
 		post_split.push(nums);
 	}
@@ -29,10 +30,7 @@ function get_columnar_nums(grid) {
 	let columnar_nums = [];
 	let max_length = grid[0].length;
 	for (let j = 0; j < max_length; j++) {
-		let num_array = [];
-		for (let k = 0; k < grid.length; k++) {
-			num_array.push(grid[k][j]);
-		}
+		let num_array = grid.map((row) => row[j]);
 
 		if (num_array.every((x) => x == " ")) {
 			continue;
@@ -47,7 +45,7 @@ function get_columnar_nums(grid) {
 let input = test.split("\n");
 let operations_str = input.pop();
 
-let operations_indices = get_indices_of_char(operations_str);
+let operations_indices = get_non_space_indices(operations_str);
 let split_nums = split_string(input, operations_indices);
 
 let operations = operations_str.split("").filter((x) => x != " ");
